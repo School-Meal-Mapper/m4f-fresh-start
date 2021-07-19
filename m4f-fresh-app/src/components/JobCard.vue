8beddb2b07523de7d2fdd2ee36b196747aa8b834
dfernandez2000 <85184614+dfernandez2000@users.noreply.github.com>
2021-07-19 12:27:40 -0400

    completed tutorial site

--- m4f-fresh-app/src/components/JobCard.vue
+++ m4f-fresh-app/src/components/JobCard.vue
@@ -2,13 +2,14 @@
     <div>
         <b-col>
             <b-card 
-            title="This Is a card" 
-            img-src="https://picsum.photos/600/300/?image=25" 
-            img-alt="Job-Info" 
-            img-top-tag="article" 
-            style="max-width:20rem" 
-            class="mb-2"
-            footer="Card Footer">
+                :title="name" 
+                img-src="https://picsum.photos/600/300/?image=25" 
+                img-alt="Job-Info" 
+                img-top-tag="article" 
+                style="max-width:20rem" 
+                class="mb-2"
+                footer="Card Footer"
+            >
                 <b-card-text>
                     quis risus sed vulputate odio ut enim blandit volutpat maecenas
                 </b-card-text>
@@ -20,7 +21,7 @@
 
 <script>
     export default {
-
+        props: ['name']
     }
 </script>
 
--- m4f-fresh-app/src/views/Home.vue
+++ m4f-fresh-app/src/views/Home.vue
@@ -2,11 +2,18 @@
   <div class="home">
     <b-container class="bv-example-row">
       <b-row align-v="center">
-        <job-card></job-card>
-        <job-card></job-card>
-        <job-card></job-card>
-        <job-card></job-card>
+        <job-card v-for="job in displayJobs" :key="job.id" :name="job.name"></job-card>
       </b-row>
+      <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      first-text="First"
+      prev-text="Prev"
+      next-text="Next"
+      last-text="Last"
+      @input="paginate(currentPage)"
+    ></b-pagination>
     </b-container>
   </div>
 </template>
@@ -18,5 +25,31 @@ import JobCard from "@/components/JobCard.vue";
 export default {
   name: "Home",
   components: { "job-card": JobCard },
+  mounted() {
+    this.fetchData();
+  },
+  data() {
+    return {
+      jobs: [],
+      displayJobs: [],
+      currentPage: 1,
+      rows: 1,
+      perPage: 3
+    };
+  },
+  methods: {
+    async fetchData() {
+      const res = await fetch("jobs.json");
+      const val = await res.json();
+      this.jobs = val;
+      this.displayJobs = val.slice(0, 3);
+      this.rows = this.jobs.length;
+      console.log(val);
+    },
+    paginate(currentPage) {
+      const start = (this.currentPage - 1) * this.perPage;
+      this.displayJobs = this.jobs.slice(start, start + 3);
+    }
+  },
 };
 </script>
