9616ffa6056f5984af9ba7b5acc5edacfc74d9ac
dfernandez2000 <85184614+dfernandez2000@users.noreply.github.com>
2021-07-21 13:37:07 -0400

    gen homepage initial branch publication

--- m4f-fresh-app/src/views/Home.vue
+++ m4f-fresh-app/src/views/Home.vue
@@ -1,59 +1,82 @@
 <template>
   <div class="home">
-    <b-container class="bv-example-row">
-      <b-row align-v="center">
-        <job-card
-          v-for="job in displayJobs"
-          :key="job.id"
-          :name="job.name"
-        ></job-card>
-      </b-row>
-      <b-pagination
-        v-model="currentPage"
-        :total-rows="rows"
-        :per-page="perPage"
-        first-text="First"
-        prev-text="Prev"
-        next-text="Next"
-        last-text="Last"
-        @input="paginate"
-      ></b-pagination>
-    </b-container>
+    <div class="blueDiv">
+      <br />
+      <h1>Welcome to M4F!</h1>
+      <h2>Connect with a Local Sponsor to Find Free Meal Sites!</h2>
+      <b-container>
+        <div class="district-buttons">
+          <b-form-select v-model="selectedState" :options="nc"
+            >Select your state.
+          </b-form-select>
+          <br />
+          <br />
+          <b-form-select v-model="selectedDistrict" :options="districtOptions" :disabled="this.selectedState !== 'nc'"
+            >Select a sponsor in your county.
+          </b-form-select>
+          <br />
+          <br />
+          <b-button class="genericSiteButton" :disabled="this.selectedDistrict === null" v-on:click="districtLink">Find Free Meals Near Me!</b-button>
+          <br />
+          <br />
+          <p><strong>OR</strong></p>
+          <div class="searchBarGroup">
+            <mdb-form-inline>
+              <input  
+              type="text" 
+              placeholder="Enter the name of your local school." 
+              aria-label="Search"
+              />
+              <mdb-btn color="mdb-color" rounded size="sm"
+                >Search</mdb-btn
+              >
+            </mdb-form-inline>
+          </div>
+          <br />
+          <br />
+        </div>
+      </b-container>
+    </div>
+    <div class="whiteDiv">
+      <p>Don't see a free meal sponsor in your county?</p>
+      <p>Learn more about our mission to connect families with free</p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import JobCard from "@/components/JobCard.vue";
-
+// import JobCard from "@/components/JobCard.vue";
+import { nc, districts } from "../constants";
 export default {
   name: "Home",
-  components: { "job-card": JobCard },
-  mounted() {
-    this.fetchData();
-  },
   data() {
     return {
-      jobs: [],
-      displayJobs: [],
-      currentPage: 1,
-      rows: 1,
-      perPage: 3,
+      nc: nc,
+      districts: districts,
     };
   },
-  methods: {
-    async fetchData() {
-      const res = await fetch("jobs.json");
-      const val = await res.json();
-      this.jobs = val;
-      this.displayJobs = val.slice(0, 3);
-      this.rows = this.jobs.length;
-      console.log(val);
-    },
-    paginate() {
-      const start = (this.currentPage - 1) * this.perPage;
-      this.displayJobs = this.jobs.slice(start, start + 3);
-    },
+  computed: {
+    districtOptions() {
+      if (this.selectedState == 'nc') {
+        return districts[this.selectedState]
+      }
+      if (this.selectedState == null) {
+        return [{ value: null, text: "You must select your state." }]
+      } else {
+        return [{ value: null, text: "You must select your state." }]
+      }
+    }
   },
 };
 </script>
+<style>
+.blueDiv {
+  color: #ffffff;
+  background-color: #0956bc;
+}
+.genericSiteButton {
+  background-color: #79b80a;
+  color: #000000;
+}
+</style>
