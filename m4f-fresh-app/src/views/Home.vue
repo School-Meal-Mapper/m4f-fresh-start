c987b2119d40c9d3e9b18e998e1037a4c61404bf
dfernandez2000 <85184614+dfernandez2000@users.noreply.github.com>
2021-07-23 09:26:32 -0400

    functional landing page components except search bar

--- m4f-fresh-app/src/views/Home.vue
+++ m4f-fresh-app/src/views/Home.vue
@@ -1,72 +1,11 @@
 <template>
-  <div class="home">
-    <div class="blueDiv">
-      <br />
-      <h1>Welcome to M4F!</h1>
-      <h2>Connect with a Local Sponsor to Find Free Meal Sites!</h2>
-      <b-container>
-        <div class="district-buttons">
-          <p>
-            <b-form-select v-model="selectedState" :options="states">Please select your state.</b-form-select>
-            <br />
-            <br />
-            <b-form-select v-model="selectedDistrict" :options="districtOptions" :disabled="this.selectedState !== 'nc'">{{
-              Please select your district.
-            </b-form-select>
-          </p>
-          <b-button :disabled="this.selectedDistrict === null" v-on:click="districtLink">{{
-            Find free meals near me!
-          </b-button>
-        </div>
-          <br />
-          <br />
-          <p><strong>OR</strong></p>
-          <div class="searchBarGroup">
-            <mdb-form-inline>
-              <input
-                type="text"
-                placeholder="Enter the name of your local school."
-                aria-label="Search"
-              />
-              <mdb-btn color="mdb-color" rounded size="sm">Search</mdb-btn>
-            </mdb-form-inline>
-          </div>
-          <br />
-          <br />
-        </div>
-      </b-container>
-    </div>
-    <div class="whiteDiv">
-      <p>Don't see a free meal sponsor in your county?</p>
-      <p>Learn more about our mission to connect families with free</p>
-    </div>
-  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { nc, districts } from "../constants";
 export default {
   name: "MFFLandingPage",
   components: {},
-  data() {
-    return {
-      nc: nc,
-      districts: districts,
-    };
-  },
-  computed: {
-    districtOptions() {
-      if (this.selectedState == "nc") {
-        return districts[this.selectedState];
-      }
-      if (this.selectedState == null) {
-        return [{ value: null, text: "You must select your state." }];
-      } else {
-        return [{ value: null, text: "You must select your state." }];
-      }
-    },
-  },
 };
 </script>
 <style>
--- m4f-fresh-app/src/views/MFFLandingPage.vue
+++ m4f-fresh-app/src/views/MFFLandingPage.vue
@@ -7,42 +7,62 @@
         <h2>Connect with a Local Sponsor to Find Free Meal Sites!</h2>
         <div class="district-buttons" id="mffGenDiv">
           <p>
-            <b-form-select v-model="selectedState" :options="states">Please select your state.</b-form-select>
-            <br />
-            <br />
-            <b-form-select v-model="selectedDistrict" :options="districtOptions" :disabled="this.selectedState !== 'nc'">
-              Please select your district.
+            <b-form-select v-model="selectedState" :options="nc" class="mb-3">
+              <b-form-select-option :value="null">Please select your state.</b-form-select-option>
             </b-form-select>
+            <b-form-select v-model="selectedDistrict" :options="districtOptions" :disabled="this.selectedState !== 'nc'" class="mb-3">
+              <b-form-select-option :value="null">Please select your district.</b-form-select-option>
+            </b-form-select>
+            <b-button 
+              class="mffGenButton" 
+              :to="{
+                name: 'SponsorLandingPage',
+                params: { sponsor: selectedDistrict, lang: $route.params.lang },
+                }"
+              :disabled="this.selectedDistrict === null">Find free meals near me!</b-button>
           </p>
-          <b-button id="mffGenButton" :disabled="this.selectedDistrict === null" v-on:click="districtLink">
-            Find free meals near me!
-          </b-button>
         </div>
-          <br />
           <p><strong>OR</strong></p>
           <div class="input-group rounded">
-            <input type="search" class="form-control rounded" placeholder="Search by the name of your local school. " aria-label="Search"
-              aria-describedby="search-addon" />
-            <b-button id="mffGenButton">
-              <b-icon icon="search"></b-icon>
-              <i class="fas fa-search"></i>
-            </b-button>
+            <b-form-input 
+              type="search" 
+              id="searchDistrictBySchoolInput"
+              class="form-control rounded" 
+              placeholder="Search by the name of your local school. " 
+              aria-label="Search"
+              aria-describedby="search-addon" 
+            ></b-form-input>
+            <b-button type="submit" class="mffGenButton">
+                <b-icon icon="search"></b-icon>
+                <i class="fas fa-search"></i>
+              </b-button>
+            
           </div>
           <br />
           <br />
       </b-container>
     </div>
     <div class="whiteDiv">
-      <br />
-      <p>Don't see a free meal sponsor in your county?</p>
+      <div class="container">
+        <p>
+          Don't see a free meal sponsor in your county?
+          <b-button class="triangleButton">
+            <b-icon icon="triangle-fill" rotate="90"></b-icon>
+          </b-button>
+        </p>
+      </div>
     </div>
     <div class="greyDiv">
-      <br />
-      <p>Learn more about our mission to connect families with free</p>
-      <br />
-    </div>
-    <div class="whiteDiv">
-      <p>Don't see a free meal sponsor in your county?</p>
+      <div class="container">
+        <p>
+          Learn more about our mission to connect families with free meals.
+          <b-button 
+            class="triangleButton"
+            href="https://www.meals4families.community">
+            <b-icon icon="triangle-fill" rotate="90"></b-icon>
+          </b-button>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -50,6 +70,7 @@
 <script>
 // @ is an alias to /src
 import { nc, districts } from "../constants";
+import sponsorData from "@/sponsorIndex";
 export default {
   name: "MFFLandingPage",
   components: {},
@@ -57,7 +78,18 @@ export default {
     return {
       nc: nc,
       districts: districts,
+      selectedState: null,
+      selectedDistrict: null,
+      sponsor: sponsorData(this.$route.params.sponsor),
     };
+  },
+  watch: {
+    "$route.params.sponsor"(to, from) {
+      if (to != from) {
+        this.sponsor = sponsorData(this.$route.params.sponsor);
+        this.refreshCSSVariables();
+      }
+    },
   },
   computed: {
     districtOptions() {
@@ -91,9 +123,11 @@ export default {
 }
 .whiteDiv {
   background-color: #ffffff;
+  color: #000000;
 }
 .greyDiv {
-  background-color: #aeaeae;
+  background-color: #e9eaeb;
+  color: #000000;
 }
 h1, h2, p {
   text-align: center;
@@ -106,9 +140,14 @@ h1, h2, p {
     background-color: #000000 !important;
   }
 }
-#mffGenButton {
+.mffGenButton {
   color: #000000;
-  background-color: #79B80A;
+  background-color: #79b80a;
   box-shadow: 0px 2px 2px;
+}
+.triangleButton {
+  color: #79b80a;
+  background-color: #ffffff00;
+  border: 0px;
 }
 </style>
