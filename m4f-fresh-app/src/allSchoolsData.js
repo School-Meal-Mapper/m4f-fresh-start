4cdac6afe4a73dac07183d2b90d624e46009fac2
dfernandez2000 <85184614+dfernandez2000@users.noreply.github.com>
2021-08-02 12:26:13 -0400

    allSchoolsData.js added with json endpoint to NC schools spreadsheet page 2

---
+++ m4f-fresh-app/src/allSchoolsData.js
@@ -0,0 +1,3 @@
+/* For now - only NC Schools */
+/* json endpoint for Copy of All_NC_Schools_July_2021 page 2 */
+var allSchoolsDataSpreadsheetURL = 'https://spreadsheets.google.com/feeds/cells/1T_hnJz75hGVfVLq8GlkZ20yNe5znKYQJdvEr3OLFht/2/public/full?alt=json';
