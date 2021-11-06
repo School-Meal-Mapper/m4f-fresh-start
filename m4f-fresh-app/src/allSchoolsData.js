bea5a242a6d0443b057c872fd5bb98086b4125b1
Kevin Chen <kekevi@github.com>
2021-11-06 15:35:47 -0400

    allSchoolsData updated to support CSV instead

--- m4f-fresh-app/src/allSchoolsData.js
+++ m4f-fresh-app/src/allSchoolsData.js
@@ -9,26 +9,23 @@ export default class allSchoolsBackend {
     /* json endpoint for Copy of All_NC_Schools_July_2021 page 2 */
     var allSchoolsDataSpreadsheetURL =
       'https://docs.google.com/spreadsheets/d/e/2PACX-1vR6RT925-6ht0fHKPbFCDZNpoUlwxaEM26GuOYJGUp7Qz97hafPgsNNqsz6SwHqnChETa_nMfMSdjWw/pub?gid=2129513394&single=true&output=csv';
-    const res = await fetchCSV(allSchoolsDataSpreadsheetURL);
-    const processed = raw.feed.entry.filter((school) => school.gsx$school_name).map((school) => school.gsx$schoolname.$t);
+    const raw = await fetchCSV(allSchoolsDataSpreadsheetURL);
+    const processed = raw.filter((school) => school.school_name).map((school) => school.school_name);
     return Array.from(processed);
   }
 
   /* returns object including each element's schoolName, sponsorName, and isOurSponsor (whether or not sponsor is on M4F (T/F) automatically filled out in sheet w data validation)) */
   static async getSchoolObject() {
     var allSchoolsDataSpreadsheetURL =
-      'https://spreadsheets.google.com/feeds/list/1T_hnJz75hGVfVLq8GlkZ20yNe5znKYQJdvEr3OLFhtw/2/public/values?alt=json';
-    const res = await fetch(allSchoolsDataSpreadsheetURL);
-    console.log(res);
-
-    const raw = await res.json();
-    const processedObj = raw.feed.entry
-      .filter((school) => school.gsx$schoolname.$t)
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vR6RT925-6ht0fHKPbFCDZNpoUlwxaEM26GuOYJGUp7Qz97hafPgsNNqsz6SwHqnChETa_nMfMSdjWw/pub?gid=2129513394&single=true&output=csv';
+    const raw = await fetchCSV(allSchoolsDataSpreadsheetURL);
+    const processedObj = raw
+      .filter((school) => school.school_name)
       .map((school) => {
         return {
-          schoolName: school.gsx$schoolname.$t,
-          sponsorName: school.gsx$leaname.$t,
-          isOurSponsor: school.gsx$isoursponsor.$t
+          schoolName: school.school_name,
+          sponsorName: school.lea_name, // school district name
+          isOurSponsor: school.is_our_sponsor
         };
       });
     return processedObj;
